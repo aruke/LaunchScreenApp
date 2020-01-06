4a837964db395e1b36866a75ea827231dd2e0242
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-01-06 20:38:15 +0900

    Add delay in launch activity

--- app/src/main/java/dev/aruke/launchscreenapp/LaunchActivity.kt
+++ app/src/main/java/dev/aruke/launchscreenapp/LaunchActivity.kt
@@ -3,6 +3,7 @@ package dev.aruke.launchscreenapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_launch.*
 
 class LaunchActivity : AppCompatActivity() {
 
@@ -10,7 +11,9 @@ class LaunchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
 
-        val mainIntent = Intent(this, MainActivity::class.java)
-        startActivity(mainIntent)
+        launchImage.postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }, 1000)
     }
 }
