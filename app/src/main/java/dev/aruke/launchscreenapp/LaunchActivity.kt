a0fddaf7da7cda101ac2cbeb296ca52c777d4120
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-01-06 20:34:09 +0900

    Added layout in launch activity

--- app/src/main/java/dev/aruke/launchscreenapp/LaunchActivity.kt
+++ app/src/main/java/dev/aruke/launchscreenapp/LaunchActivity.kt
@@ -1,12 +1,16 @@
 package dev.aruke.launchscreenapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        val mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
     }
 }
