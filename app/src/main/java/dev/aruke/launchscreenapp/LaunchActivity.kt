038cf8cade3ae09ae444265fb29011dab5bbdcac
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-02-04 00:40:31 +0900

    Added launch drawable

--- app/src/main/java/dev/aruke/launchscreenapp/LaunchActivity.kt
+++ app/src/main/java/dev/aruke/launchscreenapp/LaunchActivity.kt
@@ -2,16 +2,15 @@ package dev.aruke.launchscreenapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_launch.*
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launch)
 
-        launchImage.postDelayed({
+        Handler().postDelayed({
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
         }, 1000)
